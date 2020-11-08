a735d1d11d06a290c982b4771270216d6ce91255
acesmndr <acesmndr@gmail.com>
2020-11-08 22:17:16 +0545

    Added cursor pointer

--- src/components/Items/Tablet/Tablet.jsx
+++ src/components/Items/Tablet/Tablet.jsx
@@ -3,11 +3,11 @@ import './tablet.scss';
 
 const Tablet = () => (
   <div class="tablet">
-    <div class="tablet__front face"> </div>
-    <div class="tablet__back face"> </div>
-    <div class="tablet__right face"> </div>
-    <div class="tablet__left face"> </div>
-    <div class="tablet__top face"> </div>
+    <div class="tablet__front face"></div>
+    <div class="tablet__back face"></div>
+    <div class="tablet__right face"></div>
+    <div class="tablet__left face"></div>
+    <a href="https://medium.com/@acesmndr" referrerPolicy="no-referrer no-follow"><div class="tablet__top face"></div></a>
     <div class="tablet__bottom face"></div>
   </div>
 );
