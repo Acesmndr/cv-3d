9fc4f143125c3d456653686782f551e4ad977eda
acesmndr <acesmndr@gmail.com>
2020-11-09 11:06:39 +0545

    Add message for firefox browser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Isometric from './components/Isometric/Isometric';
 var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 
 function App() {
   return (
     <div>
-      {isSafari && <div className='safari-browser'>Currently Safari doesn't fully support this site. Please switch to chrome for better viewing experience.</div>}
+      {isSafari && <div className='safari-browser'>Currently Safari doesn't fully support this site. Please switch to Chrome for better viewing experience.</div>}
+      {isFirefox && <div className='firefox-browser'>Currently Firefox doesn't fully support this site. Please switch to Chrome for better viewing experience.</div>}
       <Isometric isSafari={isSafari} />
     </div>
   );
