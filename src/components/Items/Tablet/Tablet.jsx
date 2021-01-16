dbc783c8ad09d7994b7ef3087c249607c4c16fec
acesmndr <acesmndr@gmail.com>
2021-01-16 18:59:22 +0545

    Patch so that it looks correct in brave browser

--- src/components/Items/Tablet/Tablet.jsx
+++ src/components/Items/Tablet/Tablet.jsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import './tablet.scss';
 
 const Tablet = () => (
-  <div class="tablet">
-    <div class="tablet__front face"></div>
-    <div class="tablet__back face"></div>
-    <div class="tablet__right face"></div>
-    <div class="tablet__left face"></div>
-    <div class="tablet__top face"><a className="tablet-link" href="https://medium.com/@acesmndr" referrerPolicy="no-referrer no-follow">Medium</a></div>
-    <div class="tablet__bottom face"></div>
+  <div className="tablet">
+    <div className="tablet__front face"></div>
+    <div className="tablet__back face"></div>
+    <div className="tablet__right face"></div>
+    <div className="tablet__left face"></div>
+    <div className="tablet__top face"><a className="tablet-link" href="https://medium.com/@acesmndr" referrerPolicy="no-referrer no-follow">Medium</a></div>
+    <div className="tablet__bottom face"></div>
   </div>
 );
 
